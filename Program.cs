8ccf3021c11efb9b4ef631726d5f43c979f683e6
AdeyBoy123 <groveraden@gmail.com>
2023-09-14 15:47:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height of the diamond (half-height):");
+            int height;
+            if (int.TryParse(Console.ReadLine(), out height))
+            {
+                DrawDiamond(height);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+
+        static void DrawDiamond(int height)
+        {
+            // Upper part of the diamond (pyramid)
+            for (int i = 1; i <= height; i++)
+            {
+                // Print spaces
+                for (int j = 1; j <= height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print stars
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            // Lower part of the diamond (inverted pyramid)
+            for (int i = height - 1; i >= 1; i--)
+            {
+                // Print spaces
+                for (int j = 1; j <= height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print stars
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
